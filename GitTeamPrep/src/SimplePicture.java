6ed4bef75eac8f7586c700514d90e1b7bfdf1997
4Falcon4 <falcon@4falcon4.com>
2025-02-14 16:50:49 -0800

    tell

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -16,7 +16,7 @@ public class SimplePicture extends GraphicsProgram {
 		GImage robot = new GImage(ipath, 200, 100);
 		add(robot);
 
-		GLabel label = new GLabel("Lets go rave", 200, 400);
+		GLabel label = new GLabel("committed", 200, 400);
 
 		add(label);
 	}
